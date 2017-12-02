49ef2a307c5f4ecff0e70c871cb11dc1635b4590
MHaidarRais <32364602+MHaidarRais@users.noreply.github.com>
2017-12-02 13:31:58 +0700

    Compare your living cost in many country
    entah apps yg ke berapa

---
+++ Cost Living/Cost Living/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Cost Living
+//
+//  Created by Haidar Rais on 12/2/17.
+//  Copyright © 2017 Haidar Rais. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var webview: UIWebView!
+    //deklarasi url web
+    let urlweb = "https://www.expatistan.com/cost-of-living"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //deklarasi urlRequest
+        let requesturl = NSURL(string: urlweb)
+        
+        //deklarasi request
+        let request = NSURLRequest(url: requesturl! as URL)
+        webview.loadRequest(request as URLRequest)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
---
+++ Cost Living/Cost LivingTests/Cost_LivingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Cost_LivingTests.swift
+//  Cost LivingTests
+//
+//  Created by Haidar Rais on 12/2/17.
+//  Copyright © 2017 Haidar Rais. All rights reserved.
+//
+
+import XCTest
+@testable import Cost_Living
+
+class Cost_LivingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
